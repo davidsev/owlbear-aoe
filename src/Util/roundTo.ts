54eae66c54a04f9e59a4dfea96823f4cef06cdd9
David Severwright <code@davidsev.co.uk>
2023-07-20 22:55:23 +0100

    removed some dead code

--- src/Util/roundTo.ts
+++ src/Util/roundTo.ts
@@ -1,9 +1,3 @@
-import { Vector2 } from '@owlbear-rodeo/sdk';
-
-function instanceOfVector2 (object: any): object is Vector2 {
-    return 'x' in object && 'y' in object;
-}
-
 // Round a number to the nearest multiple of the given chunk size
 export function roundTo (value: number, chunk: number): number {
     return Math.round(value / chunk) * chunk;
